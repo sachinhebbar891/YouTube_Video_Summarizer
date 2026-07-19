16fd25e32f183fc7d94b6e9cc56eba44c0b5a285
Sachin Shankar Hebbar <sachinshebbar@Sachins-MacBook-2.local>
2026-07-19 11:50:46 +0530

    Fixed the youtube API call issue

--- src/Summarizer.py
+++ src/Summarizer.py
@@ -1,12 +1,12 @@
-from langchain_openai import ChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-llm = ChatOpenAI(
-    model="gpt-5-nano",
-    openai_api_key=os.environ["OPENAI_API_KEY"],
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-pro",
+    google_api_key=os.environ["GOOGLE_API_KEY"],
     temperature = 0.5
 )
 
